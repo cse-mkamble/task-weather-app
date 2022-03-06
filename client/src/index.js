eb4d2105529923425b86f05325d16e0e397b9f34
it-mkamble <82635318+it-mkamble@users.noreply.github.com>
2022-03-06 16:13:25 +0530

    client

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Application() {
+  return (<div>Application</div>);
+}
+
+ReactDOM.render(
+  <Application />
+  , document.getElementById('app-main'));
