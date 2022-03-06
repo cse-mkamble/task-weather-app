84f62196bab66c169518f611a8f6b95dcab81cd3
cse-mkamble <cse.mkamble@gmail.com>
2022-03-07 01:05:06 +0530

    clears

--- client/src/config/index.js
+++
@@ -1,2 +0,0 @@
-export const API_KEY = 'ea75f4e5608b77f28ed2e7aacfad09c9'
-export const API_URL = 'http://localhost:3000/data'
--- src/config/db.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    db: 'mongodb+srv://root:root@cluster0.3cbla.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-};
