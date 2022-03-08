033e6b69b7ae36f1afe644c6315027520e9c811b
cse-mkamble <cse.mkamble@gmail.com>
2022-03-08 08:25:21 +0530

    FE update

--- client/src/components/HeadBar/index.js
+++ client/src/components/HeadBar/index.js
@@ -1,22 +1,29 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { Box, Link, Toolbar, Button } from '@mui/material';
 import { logout, isAuthenticate, isAdmin } from "../../redux/actions/authAction";
 
 export default function Head() {
+    const history = useHistory();
+
     if (isAuthenticate() && isAdmin()) {
         return (<Toolbar>
-            <Link color="inherit" underline='none' href="/" sx={{ marginRight: '20px' }}>
-                Home
-            </Link>
-            <Link color="inherit" underline='none' href="/admin/dashboard" >
-                Admin
-            </Link>
+            <Button
+                size='small'
+                variant="link"
+                onClick={() => history.push("/")}
+                sx={{ marginRight: '20px' }}
+            >home</Button>
+            <Button
+                size='small'
+                variant="link"
+                onClick={() => history.push("/admin/dashboard")}
+            >home</Button>
             <Box sx={{ flexGrow: 1 }} />
             <Button
                 size='small'
                 variant="link"
                 onClick={() => logout()}
-                sx={{}}
             >logout</Button>
         </Toolbar>)
     }
